43ce27ff06f992b96afe5eccc095dc029b45e4fe
Jeremy Edward <f91jeremy@gmail.com>
2021-07-07 08:54:16 +0700

    add user_api

--- Askweb/api/urls.py
+++ Askweb/api/urls.py
@@ -2,11 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.api_overview, name="api_overview"),
-    path("view/", views.all_view, name="all_view"),
-    path("view/<int:id>/", views.question_view, name="question_view"),
-    path("userview/<int:id>/", views.user_questions_view, name="user_question_view"),
-    path("create/", views.question_create, name="question_create"),
-    path("update/<int:id>/", views.question_update, name="question_update"),
-    path("delete/<int:id>/", views.question_delete, name="question_delete")
+    path("user/", views.user_api, name="user_api"),
 ]
--- Askweb/api/views.py
+++ Askweb/api/views.py
@@ -2,58 +2,14 @@ from django.contrib.auth.models import User
 from main.models import Question
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from . serializers import QuestionSerializer
+from . serializers import QuestionSerializer, UserSerializer
 
 # Create your views here.
 
 @api_view(["GET"])
-def api_overview(request):
-    api_urls = {
-        "view": "/view/",
-        "view_detail": "/view/<int:id>/",
-        "userview": "/userview/<int:id>/",
-        "create": "/create/",
-        "update": "/update/<int:id>/",
-        "delete": "/delete/<int:id>/"
-    }
-    return Response(api_urls)
-
-@api_view(["GET"])
-def all_view(request):
-    model_data = Question.objects.all()
-    serializer = QuestionSerializer(model_data, many=True)
+def user_api(request):
+    model_data = User.objects.all()
+    serializer = UserSerializer(model_data, many=True)
     return Response(serializer.data)
 
-@api_view(["GET"])
-def question_view(request, id):
-    model_data = Question.objects.get(id=id)
-    serializer = QuestionSerializer(model_data)
-    return Response(serializer.data)
-
-@api_view(["GET"])
-def user_questions_view(request, id):
-    user = User.objects.get(id=id)
-    model_data = user.question_set.all()
-    serializer = QuestionSerializer(model_data, many=True)
-    return Response(serializer.data)
-
-@api_view(["POST"])
-def question_create(request):
-    serializer = QuestionSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
-
-@api_view(["POST"])
-def question_update(request, id):
-    model_data = Question.objects.get(id=id)
-    serializer = QuestionSerializer(instance=model_data, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
-
-@api_view(["DELETE"])
-def question_delete(request, id):
-    model_data = Question.objects.get(id=id)
-    model_data.delete()
-    return Response("item deleted")
+    
